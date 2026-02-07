51bdbaa9c97c8b00a7ec1fa3568fea9b7c41c1d4
Shabeer Ahmad Ahsas <ahsas.25786.kamal@gmail.com>
2026-02-07 19:24:14 +0430

    Delete src/report_sections directory

--- src/report_sections/__init__.py
+++
@@ -1,7 +0,0 @@
-# src/report_sections/__init__.py
-"""
-Report sections package.
-
-This package intentionally avoids importing section modules at import-time
-to prevent heavy imports and circular dependencies (Streamlit-friendly).
-"""
